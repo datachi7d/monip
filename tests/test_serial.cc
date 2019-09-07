c20a4c58bdbf3068d395c164aa13138c58231244
Sean Kelly <seandtkelly@gmail.com>
2019-09-07 22:21:20 +1200

    WIP: Cleanup and add retry test

--- tests/test_serial.cc
+++ tests/test_serial.cc
@@ -56,7 +56,8 @@ protected:
 			char pty2[256] = {0};
 			sprintf(pty2, "pty,raw,echo=0,link=%s", SerialPath.c_str());
 
-			std::vector<const char *> commandVector { "/usr/bin/socat", "-d", "-d", "-d", "-d", "-D", "-lu", pty1, pty2 };
+			//"-d", "-d", "-d", "-d",
+			std::vector<const char *> commandVector { "/usr/bin/socat",  "-D", "-lu", pty1, pty2 };
 			_pid = SpawnProcess(commandVector, false, false);
 
 			int counter = 0;
@@ -167,6 +168,8 @@ protected:
 		SerialStream.flush();
 
 		ASSERT_EQ(ReadMessage(serial, AUTOREPORT_HEADER, testReadData), 28);
+
+		printf("%s\n", ConvertAutoReportToJSON((AutoReportMessage *)&testReadData[0]));
 	}
 
 	TEST_F(SerialTest, test_ReadMessage_Checksum_Fail)
@@ -184,63 +187,45 @@ protected:
 		ASSERT_EQ(ReadMessage(serial, AUTOREPORT_HEADER, testReadData), -EIO);
 	}
 
-	class Message : public testing::Test
-	{
-	
-	};
 
-	TEST_F(Message, test_Message)
+	TEST_F(SerialTest, test_ReadMessage_BadData)
 	{
-		
-		//uint8_t buffer[256] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x01, 0x01, 0x00, 0x00 };
-		//uint8_t buffer[256] = { 0xc7, 0x44, 0x00, 0xb0, 0xac, 0xff, 0xdb, 0xa8, 0x03, 0x14, 0x11, 0x00, 0x12, 0x00, 0x00, 0xee, 0xff, 0xff, 0x0b0, 0x00, 0x08, 0x5c, 0x10, 0x00, 0x00, 0x00, 0x0d };
-		uint8_t buffer[256] = { 0xdc, 0x4c, 0x00 , 0xc1, 0xac, 0xff , 0xdd, 0xa8, 0x03 , 0x51, 0x11, 0x00 , 0xe9, 0xff, 0xff , 0xec, 0xff, 0xff , 0xf2, 0xff, 0xff , 0x85, 0xc1, 0x00 , 0x00, 0x00, 0x00 };
-		printf("%s\n", ConvertAutoReportToJSON((AutoReportMessage *)&buffer[0]));
+		Serial * serial = Serial_New(SerialPath.c_str());
+
+		EXPECT_TRUE(serial != NULL);
+
+		unsigned char writeData[] = {0xae, 0x1e, 0xdc, 0x4c, 0x00 , 0xc1, 0xac, 0xff , 0xdd, 0xa8, 0x03 , 0x51, 0x11, 0x00 , 0xe9, 0xff, 0xff , 0xec, 0xff, 0xff , 0xf2, 0xff, 0xff , 0x85, 0xc1, 0x00 , 0x00, 0x00, 0x00, 0xaf};
+		uint8_t testReadData[255] = {0};
+
+		unsigned int i = 0;
+
+		int Result = 0;
+		int Counter = 0;
+		srand(time(NULL));
+
+		do {
+			printf("start...\n");
+			if(Counter == 0)
+			{
+				for(i = 0; i < 255; i++)
+				{
+					unsigned char random = rand();
+					SerialStream.write((char *)&random, 1);
+				}
+				SerialStream.flush();				
+			}
+			else
+			{
+				SerialStream.write((char *)writeData, sizeof(writeData));
+				SerialStream.flush();				
+			}
+			
+			Result = ReadMessage(serial, AUTOREPORT_HEADER, testReadData);
+			printf("Counter:%d Result:%d\n", Counter, Result);
+			Counter++;
+		} while (Counter < 100 && Result != 28);
+
+		ASSERT_EQ(Result, 28);
 	}
-	// TEST_F(PFC_Serial, test_Serial_ReadMessage)
-	// {
-	// 	Serial * serial = Serial_New(SerialPath.c_str());
-
-	// 	EXPECT_TRUE(serial != NULL);
-
-	// 	char writeData[] = {0xf6, 0x03, 0x00, 0x06};
-	// 	uint8_t testReadData[255] = {0};
-
-	// 	SerialStream.write(writeData, sizeof(writeData));
-	// 	SerialStream.flush();
-
-	// 	PFC_ID ID = 0;
-	// 	pfc_size size = 0;
-
-	// 	ASSERT_EQ(Serial_ReadPFCMessage(serial, &ID, testReadData, &size), PFC_ERROR_NONE);
-
-	// 	ASSERT_EQ(size, 1);
-	// 	ASSERT_EQ(ID, 0xf6);
-	// 	ASSERT_EQ(testReadData[0], 0);
-
-	// 	Serial_Free(serial);
-	// }
-
-	// TEST_F(PFC_Serial, test_Serial_WriteMessage)
-	// {
-	// 	Serial * serial = Serial_New(SerialPath.c_str());
-
-	// 	EXPECT_TRUE(serial != NULL);
-
-	// 	char expectedData[] = {0xf6, 0x03, 0x00, 0x06};
-	// 	char testReadData[255] = {0};
-
-	// 	PFC_ID ID = 0xf6;
-	// 	pfc_size size = 1;
-	// 	uint8_t writeData = 0;
-
-	// 	ASSERT_EQ(Serial_WritePFCMessage(serial, ID, &writeData, size), PFC_ERROR_NONE);
-
-	// 	ASSERT_USECS(SerialStream.read(testReadData, sizeof(expectedData)), 100000);
-
-	// 	ASSERT_TRUE(memcmp(expectedData, testReadData, sizeof(expectedData)) == 0);
-
-	// 	Serial_Free(serial);
-	// }
 
 }
